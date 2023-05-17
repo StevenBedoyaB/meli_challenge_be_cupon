7ab292e622633ac02a50dcabf118e12db6c7b0d2
Steven Bedoya <steven.bedoyab@gmail.com>
2023-05-17 14:55:37 -0500

    Type fix

--- TypeScript/src/models/favorites.model.ts
+++ TypeScript/src/models/favorites.model.ts
@@ -3,6 +3,6 @@ import { DataTypes } from 'sequelize';
 module.exports = (sequelize) => {
     return sequelize.define('favorites', {
         item_id: DataTypes.STRING,
-        freq: DataTypes.INTEGER
+        freq: DataTypes.FLOAT
     })
 };
